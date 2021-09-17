841812f780a6481210989c9893a1376e87916e99
mercyplaisir <kirangimercyplaisir@gmail.com>
2021-09-17 09:47:43 +0200

    --

--- src/main.py
+++ src/main.py
@@ -24,23 +24,27 @@ def createDailyFolder(folderDestination):
     if dateToday not in os.listdir(folderDestination):
         newFolder = f'{folderDestination}/{dateToday}'
         os.mkdir(newFolder)
+        print("created a folder ",newFolder)
         return newFolder
     elif dateToday in os.listdir(folderDestination):
         return f'{folderDestination}/{dateToday}'
 
 def CreatePlaylistFolder(folderDestination):
-
+    
     idPlaylist = [int(i.split('playlist')[1]) for i in os.listdir(folderDestination) if 'playlist' in i]
     idPlaylist.sort()
     if len(idPlaylist)>0:
         nextId = idPlaylist[len(idPlaylist)-1]+1
         newPlaylistFolder = f'{folderDestination}/playlist{nextId}'
         os.mkdir(newPlaylistFolder)
+        print(">>>Created a folder ",newPlaylistFolder)
         return newPlaylistFolder
     else:
         os.mkdir(f'{folderDestination}/playlist1')
+
+        print(f'created  a folder {folderDestination}/playlist1')
         return f'{folderDestination}/playlist1'
-        
+    print(2)
 #=================================================================================================
 
 
@@ -48,31 +52,34 @@ def downloadPlaylistVideo(url,destinationFolder = videoDestination):
     #youtube-dl --max-filesize 50M <playlist_url>
     print("download Video only from playlist")
     newDailyFolder=createDailyFolder(destinationFolder)
-    PlaylistFolder = CreatePlaylistFolder(newDailyFolder)
+    playlistFolder = CreatePlaylistFolder(newDailyFolder)
 
-    os.system(f'youtube-dl -o {playlistFolder} -f \'best[height<720]\'  {url}') #--max-filesize 50MiB    
+    os.system(f"youtube-dl --yes-playlist --format mp4 -o '{playlistFolder}/%(title)s.%(ext)s' '{url}'") #--max-filesize 50MiB    
 
 def downloadPlaylistAudio(url,destinationFolder = musicDestination):
     #youtube-dl --yes-playlist --extract-audio <playlist_url>
 
     newDailyFolder=createDailyFolder(destinationFolder)#create a today Folder
-    PlaylistFolder = CreatePlaylistFolder(newDailyFolder)#create a playlist Folder
+    playlistFolder = CreatePlaylistFolder(newDailyFolder)#create a playlist Folder
+    print(f"Stored to {playlistFolder} ")
 
     print("download audio only from playlist")
-    os.system(f'youtube-dl --yes-playlist -o {playlistFolder} --extract-audio {url}')
+    os.system(f'youtube-dl --yes-playlist -o "{playlistFolder}/%(title)s.%(ext)s" --extract-audio "{url}"')
 
 def downloadAudio(url,destinationFolder = musicDestination):
     #youtube-dl -x https://www.youtube.com/watch?v=7E-cwdnsiow
     folderDestination = createDailyFolder(destinationFolder)#create a folder of today's date
-    print("downloading Audio")
-    os.system(f'youtube-dl -x --audio-format mp3 -o \"{folderDestination}/%(title)s.%(ext)s\" {url}')
+    print(f"downloading Audio\n Stored to {folderDestination}")
+
+
+    os.system(f'youtube-dl -x --audio-format mp3 -o \"{folderDestination}/%(title)s.%(ext)s\" "{url}"')
 
 
 def downloadVideo(url,destinationFolder = videoDestination):
     #youtube-dl -f 22 https://www.youtube.com/watch?v=7E-cwdnsiow
     folderDestination = createDailyFolder(destinationFolder)#create a folder of today's date
-    print("downloading Video")
-    os.system(f'youtube-dl -f \'best[height<720]\' -o \"{folderDestination}/%(title)s.%(ext)s\" {url}')#--max-filesize 50M
+    print(f"downloading Video \n Stored to {folderDestination}")
+    os.system(f'youtube-dl --format mp4 -o \"{folderDestination}/%(title)s.%(ext)s\" "{url}"')#--max-filesize 50M
 
 
 
@@ -87,6 +94,8 @@ def ChooseType():
 
 
 
+#main
+
 
 
 
@@ -97,21 +106,24 @@ else:
     print("c'est une video\n")
     isPlaylist=False
 
+
+
+
 decision = ChooseType()
 
 
 try:
-    if decision==1 :
+    if decision==1 :#video
         if isPlaylist:
             downloadPlaylistVideo(ytbLink)
         elif not isPlaylist:
             downloadVideo(ytbLink)
-    if decision==2:
+    if decision==2:#audio
         if isPlaylist:
             downloadPlaylistAudio(ytbLink)
         elif not isPlaylist:
             downloadAudio(ytbLink)
 
     print("Download Finished")
-except:
-    pass
+except Exception as e:
+    print(e)
